a45b5e2f4609b16c616da3e1794ba9076d7dc19f
NgoniDukeMoyo <dukemoyo7@gmail.com>
2021-06-18 04:21:56 +0000

    final connect emailjs to booking page still error

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,14 @@
-console.log("hello");
+function sendEmail(contactForm) {
+    emailjs.send("gmail","Duke Moyo Template",{
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailAaddress.value,
+        "project_request": contactForm.projectsummary.value,
+    })
+    .then(
+        function(response){
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        })
+}
