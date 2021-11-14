7bcc3e6e91c1845774effb754b982c3e6b972193
NgoniDukeMoyo <dukemoyo7@gmail.com>
2021-11-14 19:00:03 +0000

    change map id color for the pop-up text to come through

--- assets/js/mapsapi.js
+++ assets/js/mapsapi.js
@@ -13,52 +13,52 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), options);
   
     //duke attempt map
-    var marker = addMarker({
+    var markers = [{
         coods: {
             lat: 52.63595,
             lng: 4.73622,
         },
         iconImage: "http://maps.google.com/mapfiles/kml/pal3/icon48.png",
-        infoText: "<p> url: https://www.airbnb.com/rooms/36616246?translate_ugc=false&federated_search_id=bf8cacc9-3df5-4805-bb09-255765468282&source_impression_id=p3_1627050299_2WskDflabalWA29%2B&guests=1&adults=1 </p>",
-    });
+        infoText: " <a href='https://www.airbnb.com/rooms/36616246?translate_ugc=false&federated_search_id=bf8cacc9-3df5-4805-bb09-255765468282&source_impression_id=p3_1627050299_2WskDflabalWA29%2B&guests=1&adults=1'>Home1</a>",
+    },
   
-    var marker2 = addMarker({
+    {
         coods: {
             lat: 52.63657,
             lng: 4.75619,
         },
         iconImage: "http://maps.google.com/mapfiles/kml/pal3/icon48.png",
         infoText: "<p> url: https://www.airbnb.com/rooms/43590586?translate_ugc=true&federated_search_id=bf8cacc9-3df5-4805-bb09-255765468282&source_impression_id=p3_1627050148_EbY041kBbAXwQiW2&guests=1&adults=1 </p>",
-    });
+    },
   
-    var marker3 = addMarker({
+    {
         coods: {
             lat: 52.6378,
             lng: 4.7408,
         },
         iconImage: "http://maps.google.com/mapfiles/kml/pal3/icon48.png",
         infoText: "<p> url: https://www.airbnb.com/rooms/50113014?translate_ugc=true&federated_search_id=bf8cacc9-3df5-4805-bb09-255765468282&source_impression_id=p3_1627050215_HMdYkj3m1L%2F8Ucei&guests=1&adults=1 </p>",
-    });
+    },
   
-    var marker4 = addMarker({
+    {
         coods: {
             lat: 52.6438,
             lng: 4.7638,
         },
         iconImage: "http://maps.google.com/mapfiles/kml/pal3/icon48.png",
         infoText: "<p> url: https://www.airbnb.com/rooms/9866644?translate_ugc=false&federated_search_id=bf8cacc9-3df5-4805-bb09-255765468282&source_impression_id=p3_1627050257_nWq5OAljXe90z%2BtC&guests=1&adults=1 </p>",
-    });
+    },
   
-    var marker5 = addMarker({
+    {
         coods: {
             lat: 52.6373,
             lng: 4.7527,
         },
         iconImage: "http://maps.google.com/mapfiles/kml/pal3/icon48.png",
         infoText: "<p> url: https://www.airbnb.com/rooms/36616246?translate_ugc=false&federated_search_id=bf8cacc9-3df5-4805-bb09-255765468282&source_impression_id=p3_1627050299_2WskDflabalWA29%2B&guests=1&adults=1 </p>",
-    });
+    }];
   
-  
+    markers.forEach(element=>addMarker(element));
   
     //duke attempt marker function
     function addMarker(props) {
